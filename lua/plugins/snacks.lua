2a034fa42fc4bd9df8c32b2496c7c6ac878ca6d3
François <25376055+fravan@users.noreply.github.com>
2025-08-05 11:35:27 +0200

    Removed bufferline
    Also made snacks prompt close on <Esc> key

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -35,7 +35,16 @@ return {
 		explorer = { enabled = true },
 		indent = { enabled = true },
 		input = { enabled = true },
-		picker = { enabled = true },
+		picker = {
+			enabled = true,
+			win = {
+				input = {
+					keys = {
+						["<Esc>"] = { "close", mode = { "n", "i" } },
+					},
+				},
+			},
+		},
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
